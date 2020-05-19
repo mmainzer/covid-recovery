a0430aff9a3948260fff734b0bb66afe689ea2ea
mmainza <mike.mainzer@gmail.com>
2020-05-19 13:42:01 -0400

    cleaned data file
    Added categorical date for bar chart

---
+++ application/jobs/js/charts.js
@@ -0,0 +1,4 @@
+// set the dimensions and margins for the chart
+let margin = {top: 10, right: 30, bottom: 30, left: 60};
+let height = $(window).height() * .5;
+let width = $(window).width() * .45;
